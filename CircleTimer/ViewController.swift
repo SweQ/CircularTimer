12f79b34beab0c2f0b5b68d495dea1bba83e21f0
Alex Koro <renfer@yandex.ru>
2021-12-04 18:11:26 +0300

    Initial

---
+++ CircleTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CircleTimer
+//
+//  Created by alexKoro on 4.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
